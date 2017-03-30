f7c5b0e92cfb7dc4b35738f627109a0050c81f42
vikey <chenyuanqi@ltbl.cn>
2017-03-30 15:00:01 +0800

    增加 update mapping、bulk 命令行操作

--- src/Commands/ElasticsearchService.php
+++ src/Commands/ElasticsearchService.php
@@ -5,26 +5,43 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use chenyuanqi\elasticSearchService\Index;
 
+/**
+ * Elasticsearch Service 命令行
+ *
+ * @package App\Console\Commands
+ */
 class ElasticsearchService extends Command
 {
     /**
-     * The name and signature of the console command.
-     * $indexType 索引配置名
-     * $action 操作名 [new bulk clear]
+     * 命令名称及参数
+     * name 为索引配置名称，action 为操作名称 [new update bulk clear]
      *
      * @var string
      */
     protected $signature = 'elastic:search {name} {action}';
 
     /**
-     * The console command description.
+     * 命令描述
      *
      * @var string
      */
     protected $description = 'elasticsearch 批量索引操作';
 
     /**
-     * Create a new command instance.
+     * 索引配置名称
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * 操作名称
+     *
+     * @var
+     */
+    protected $action;
+    /**
+     * 创建命令行实例
      *
      * @return void
      */
@@ -40,7 +57,7 @@ class ElasticsearchService extends Command
      */
     public function handle()
     {
-        $start = microtime(true);
+        $start        = microtime(true);
         $this->name   = $this->argument('name');
         $this->action = $this->argument('action');
 
@@ -52,12 +69,20 @@ class ElasticsearchService extends Command
                     $this->mapping();
                     break;
 
+                case 'update':
+                    $this->updateMapping();
+                    break;
+
+                case 'bulk':
+                    $this->bulk();
+                    break;
+
                 case 'clear':
                     $this->clear();
                     break;
 
                 default:
-                    $this->error('操作 '.$this->action.' 未发现，目前仅支持 new, clear 操作');
+                    $this->error('操作 '.$this->action.' 未发现，目前仅支持 new, update, clear 操作');
                     break;
             }
         }
@@ -77,6 +102,52 @@ class ElasticsearchService extends Command
         $index = new Index($this->name);
         $index->mapping();
         $this->info('新建索引结束');
+    }
+
+    /**
+     * 更新索引操作
+     *
+     * @return void
+     */
+    protected function updateMapping()
+    {
+        $this->info('更新索引开始');
+
+        $this->info('更新索引结束');
+    }
+
+    /**
+     * 批量写入数据
+     *
+     * @return void
+     */
+    protected function bulk()
+    {
+        $this->info('批量写入数据开始');
+        $index = new Index($this->name);
+        $model    = new $index->config['model'];
+        $limit= array_get($index->config, 'limitNum', 1000);
+
+        $model->chunk($limit, function ($datas){
+            $params = ['body' => []];
+            foreach ($datas as $row) {
+
+                $data = [
+                    'index' => [
+                        '_index' => $index->index,
+                        '_type'  => $index->type,
+                    ]
+                ];
+
+                $data['index']['_id'] = sprintf('%s%s', $row->id, $row->apps_type);
+                $params['body'][]     = $data;
+                $params['body'][]     = $index->getItemBody($row);
+            }
+
+            $index->bulk($params);
+        });
+
+        $this->info('批量写入数据结束');
     }
 
     /**
