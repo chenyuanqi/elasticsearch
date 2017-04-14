485c22dc0621f7845d028b08c6f2d0f12f69b86d
chenyuanqi <chenyuanqi90s@163.com>
2017-04-14 19:15:04 +0800

    增加批量复制的方法

--- src/Commands/ElasticsearchService.php
+++ src/Commands/ElasticsearchService.php
@@ -18,7 +18,7 @@ class ElasticsearchService extends Command
      *
      * @var string
      */
-    protected $signature = 'elastic:search {name} {type} {action}';
+    protected $signature = 'elastic:search {name} {type} {action} {copy_name?} {?copy_type?}';
 
     /**
      * 命令描述
@@ -85,12 +85,18 @@ class ElasticsearchService extends Command
                     $this->bulk();
                     break;
 
+                case 'copy':
+                    $name = $this->name = $this->argument('copy_name');
+                    $type = $this->type = $this->argument('copy_type');
+                    $this->copy($name, $type);
+                    break;
+
                 case 'clear':
                     $this->clear();
                     break;
 
                 default:
-                    $this->error('操作 '.$this->action.' 未发现，目前仅支持 new, update, bulk, clear 操作');
+                    $this->error('操作 '.$this->action.' 未发现，目前仅支持 new, update, bulk, copy, clear 操作');
                     break;
             }
         }
@@ -122,6 +128,7 @@ class ElasticsearchService extends Command
         $this->info('更新映射开始');
         $index = (new Builder)->index($this->name)->type($this->type);
         $index->updateMapping();
+        $index->debug();
         $this->info('更新映射结束');
     }
 
@@ -149,6 +156,49 @@ class ElasticsearchService extends Command
         });
 
         $this->info('批量写入数据结束');
+    }
+
+    /**
+     * 复制索引操作
+     *
+     * @param  string $name 要复制的索引名称
+     * @param  string $type 要复制的索引类型
+     *
+     * @return void
+     */
+    protected function copy($name, $type)
+    {
+        if(!$name || !$type) {
+            $this->error('批量复制的索引名称及类型参数不能为空！');
+            exit();
+        }
+
+        $this->info('批量复制数据开始');
+        $targetIndex = (new Builder)->index($name)->type($type);
+        $targetData  = $targetIndex->scroll(1000, '30s', 'scan')->search();
+
+        while (true) {
+            $scrollId = $targetData['_scroll_id'];
+
+            // 移除多余参数，便于 bulk 批量写入数据
+            array_pull($targetData, '_scroll_id');
+            array_pull($targetData, 'total');
+
+            // 批量复制
+            $needle = collect($targetData)->map(function ($item){
+                $item[0] = 'index';
+
+                return $item;
+            })->toArray();
+            (new Builder)->index($this->name)->type($this->type)->bulk($needle);
+
+            $targetData = $targetIndex->searchByScrollId($scrollId)->outputFormat();
+            if (!$targetData) {
+                break;
+            }
+        }
+
+        $this->info('批量复制数据结束');
     }
 
     /**
