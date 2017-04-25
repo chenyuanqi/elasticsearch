9680d02fa1643fe5ab3fc5e90a79fbf76574f7cd
chenyuanqi <chenyuanqi90s@163.com>
2017-04-25 16:05:18 +0800

    补全 crud 测试用例，并使用数据提供器

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -22,14 +22,110 @@ final class QueryTest extends TestCase
 
     /**
      * 测试写入数据
+     *
+     * @param string $id
+     * @param string $title
+     * @param string $content
+     *
+     * @dataProvider additionProvider
+     * @group        query-crud
      */
-    public function testInsert()
+    public function testInsert($id, $title, $content)
     {
         $data = [
-            'title'   => str_random(6).'國際航空電視臺',
-            'content' => str_random(30)
+            'title'   => $title,
+            'content' => $content
         ];
-        $result = $this->index->insert($data);
-        self::assertTrue($result['created']);
+
+        $result = $this->index->insert($data, $id);
+        self::assertEquals(0, $result['_shards']['failed']);
+    }
+
+    /**
+     * 测试根据 ID 更新数据
+     *
+     * @param string $id
+     * @param string $title
+     * @param string $content
+     *
+     * @dataProvider additionProvider
+     * @group        query-crud
+     */
+    public function testUpdateById($id, $title, $content)
+    {
+        $data = [
+            'title'   => $title,
+            'content' => $content
+        ];
+
+        $result = $this->index->updateById($data, $id);
+        self::assertEquals(0, $result['_shards']['failed']);
+    }
+
+    /**
+     * 测试根据 ID 更新数据
+     *
+     * @param string $id
+     * @param string $title
+     * @param string $content
+     *
+     * @dataProvider additionProvider
+     * @group        query-crud
+     */
+    public function testInsertOrUpdate($id, $title, $content)
+    {
+        $data = [
+            'title'   => $title,
+            'content' => $content
+        ];
+
+        $result = $this->index->insertOrUpdate($data, $id);
+        self::assertEquals(0, $result['_shards']['failed']);
+    }
+
+    /**
+     * 测试根据 ID 删除数据
+     *
+     * @param string $id
+     *
+     * @dataProvider additionIdProvider
+     * @group        query-crud
+     */
+    public function testDeleteById($id)
+    {
+        $result = $this->index->deleteById($id);
+        self::assertEquals(0, $result['_shards']['failed']);
+    }
+
+    /**
+     * _source 数据提供器
+     *
+     * @return array
+     */
+    public function additionProvider()
+    {
+        return [
+            'One'   => ['1', '國際航空電視臺1', 'content1'.str_random(6)],
+            'Two'   => ['2', '國際航空電視臺2', 'content2'.str_random(6)],
+            'Three' => ['3', '國際航空電視臺3', 'content3'.str_random(6)],
+            'Four'  => ['4', '國際航空電視臺4', 'content4'.str_random(6)],
+            'Five'  => ['5', '國際航空電視臺5', 'content5'],
+        ];
+    }
+
+    /**
+     * id 数据提供器
+     *
+     * @return array
+     */
+    public function additionIdProvider()
+    {
+        return [
+            'One'   => ['1'],
+            'Two'   => ['2'],
+            'Three' => ['3'],
+            'Four'  => ['4'],
+            'Five'  => ['5'],
+        ];
     }
 }
