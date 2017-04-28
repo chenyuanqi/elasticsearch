86242db03487580dce47162ba75bba9a471e62ca
chenyuanqi <chenyuanqi90s@163.com>
2017-04-28 11:10:22 +0800

    修改配置分组

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -27,6 +27,7 @@ final class ConfigTest extends TestCase
      */
     public function testCreateTemplate()
     {
+        echo __METHOD__."\n";
         $templateName = 'default_template';
         $result       = $this->index->createTemplate($templateName);
         self::assertTrue($result['acknowledged']);
@@ -39,6 +40,7 @@ final class ConfigTest extends TestCase
      */
     public function testDeleteTemplate()
     {
+        echo __METHOD__."\n";
         $templateName = 'default_template';
         $result       = $this->index->deleteTemplate($templateName);
         self::assertTrue($result['acknowledged']);
@@ -47,11 +49,14 @@ final class ConfigTest extends TestCase
     /**
      * 测试建立映射
      *
-     * @group config-special
+     * @group config-normal
      */
     public function testCreateMapping()
     {
-        $result = $this->index->createMapping();
+        echo __METHOD__."\n";
+        $indexName = 'default';
+        $typeName  = 'default';
+        $result = $this->index->index($indexName)->type($typeName)->createMapping();
         self::assertTrue($result['acknowledged']);
     }
 
@@ -62,7 +67,64 @@ final class ConfigTest extends TestCase
      */
     public function testClearIndex()
     {
+        echo __METHOD__."\n";
         $result = $this->index->truncate();
+        self::assertTrue($result['acknowledged']);
+    }
+
+    /**
+     * 测试建立别名
+     *
+     * @group config-normal
+     */
+    public function testCreateAlias()
+    {
+        echo __METHOD__."\n";
+        $aliasName = 'df_v1';
+        $result    = $this->index->createAlias($aliasName);
+        self::assertTrue($result['acknowledged']);
+    }
+
+    /**
+     * 测试别名是否存在
+     *
+     * @group config-normal
+     */
+    public function testIsAlias()
+    {
+        echo __METHOD__."\n";
+        $aliasName = 'df_v1';
+        $result    = $this->index->isAlias($aliasName);
+        self::assertTrue($result);
+    }
+
+    /**
+     * 测试迁移数据
+     *
+     *
+     * @group   config-normal
+     */
+    public function testMigrateIndex()
+    {
+        //@depends testIsAlias
+        echo __METHOD__."\n";
+        $aliasName = 'df_v1';
+        $newIndex  = 'default_migrate';
+        $result = $this->index->migrateIndex($aliasName, $newIndex);
+        self::assertTrue($result['acknowledged']);
+    }
+
+    /**
+     * 测试删除别名
+     *
+     * @group config-special
+     */
+    public function testDeleteAlias()
+    {
+        echo __METHOD__."\n";
+        $aliasName = 'df_v1';
+        $indexName = 'default_migrate';
+        $result    = $this->index->index($indexName)->deleteAlias($aliasName);
         self::assertTrue($result['acknowledged']);
     }
 
@@ -73,6 +135,7 @@ final class ConfigTest extends TestCase
      */
     public function testGetShardsNumber()
     {
+        echo __METHOD__."\n";
         $shardsNumber = 3;
         self::assertEquals($shardsNumber, $this->index->getShardsNumber());
     }
@@ -84,6 +147,7 @@ final class ConfigTest extends TestCase
      */
     public function testGetLimitByConfig()
     {
+        echo __METHOD__."\n";
         $limit = 10000;
         self::assertEquals($limit, $this->index->getLimitByConfig());
     }
@@ -95,6 +159,7 @@ final class ConfigTest extends TestCase
      */
     public function testGetModel()
     {
+        echo __METHOD__."\n";
         $modelName = '\App\Model\Default';
         self::assertInstanceOf($modelName, $this->index->getModel());
     }
